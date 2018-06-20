fec808637019f166f31d360e178675da1c36865d
Dorcas Mbi <24913040+dorcasmbi@users.noreply.github.com>
2018-06-20 23:11:49 +0200

    Updated Exam Class

--- uniframework/src/exams/Exam.scala
+++ uniframework/src/exams/Exam.scala
@@ -1,12 +1,51 @@
 package uniframeWork
 
+/**
+  * Created by Dorcas
+  * Exam class version 1.1
+  */
+class Exam(val subject_name:String, val venue_no:String, val exam_time:String, val exam_controller:String, val exam_date:String)
+{
+  var subName: String = subject_name
+  var venueNo: String = venue_no
+  var examTime: String = exam_time
+  var examDate: String = exam_date
+  var examController: String = exam_controller
+
+  def getSubjectName(): String = {
+       return this.subName;
+     }
+
+  def getVenueNo() : String = {
+    return this.venueNo
+  }
+
+  def getExamTime() : String = {
+    return this.examTime
+  }
+
+  def getExamDate() : String = {
+    return this.examDate
+  }
+
+  def getExamController() : String = {
+    return this.examController
+  }
+
+  def ExamDetails() =
+  {
+    println("Subject : " +getSubjectName() )
+    println("Venue no: " +getVenueNo())
+    println("Date: " +getExamDate())
+    println("Time: " +getExamTime())
+    println("Controller: " +getExamController())
+  }
+}
 object Exam extends App
 {
 
+  var exam = new Exam("ADP",venue_no = "1.35", exam_time = "2 PM",exam_date = "17 June",exam_controller = "RothMan")
 
-  def setExamDetails { println("Exam details set") }
-
-  def getExamDetails { println("Exam details got") }
-
+  exam.ExamDetails()
 
 }
