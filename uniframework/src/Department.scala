723f7d22e5013025d9f8ab0c0e553195562c8410
Dorcas Mbi <24913040+dorcasmbi@users.noreply.github.com>
2018-06-27 16:49:07 +0200

    Department Class model

---
+++ uniframework/src/Department.scala
@@ -0,0 +1,21 @@
+package uniframeWork
+
+class Department()
+{
+
+  var department_id = ""
+  var department_name = "Information and Communication Technology"
+  var department_building = ""
+
+
+}
+
+object Department extends App()
+{
+  var department = new Department()
+
+
+  println("Department name : "+department.department_name)
+
+}
+
