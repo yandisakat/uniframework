87a051c19e0a272bba340f87f2262138e98dbf4a
MillerThomas <33414062+MillerThomas@users.noreply.github.com>
2018-07-19 13:59:50 +0200

    Update course.scala

--- uniframework/src/course.scala
+++ uniframework/src/course.scala
@@ -1,51 +1,46 @@
-class course(val course_ID:Int,val dept_ID:Int,val course_Title:String,val course_Descript:String,
-           val course_Credit:String,val course_Level:String,val course_Duration:String)
+/**
+* Created by 216074630 on 6/20/2018.
+* @param  course id
+* @param department id
+* @param course title
+* @param course descript 
+* @param course credit
+* @param course level
+* @param course duration
+*/
+class course(
+    var course_ID:Int =0,
+    var dept_ID:Int = 0,
+    var course_Title:String = "",
+    var course_Descript:String = "",
+    var course_Credit:String = "",
+    var course_Level:String = "",
+    var course_Duration:String = "")
 {
 
-  var courseID: Int = course_ID
-  var deptID: Int = dept_ID
-  var courseTitle: String = course_Title
-  var courseDescript: String = course_Descript
-  var courseCredit: String = course_Credit
-  var courseLevel: String = course_Level
-  var courseDuration: String = course_Duration
-
-  def getCourseID(): Int = {
-    return this.courseID;
-  }
-
-  def getDeptID(): Int = {
-    return this.deptID;
-  }
-
-  def getTitle(): String = {
-    return this.courseTitle;
-  }
-
-  def getDescript(): String = {
-    return this.courseDescript;
-  }
-
-  def getCredit(): String = {
-    return this.courseCredit;
-  }
-
-  def getLevel(): String = {
-    return this.courseLevel;
-  }
-
-  def getDuration(): String = {
-    return this.courseDuration;
-  }
+  def courseID(value: Int):Unit = course_ID = value
+  def deptID(value: Int):Unit = dept_ID = value
+  def courseTitle(value: String): Unit = course_Title = value
+  def courseDescript(value: String): Unit = course_Descript = value
+  def courseCredit(value: String): Unit = course_Credit = value
+  def courseLevel(value: String): Unit = course_Level = value
+  def courseDuration(value: String): Unit = course_Duration = value
+  def getCourseID = course_ID;
+  def getDeptID = dept_ID;
+  def getTitle = course_Title;
+  def getDescript = course_Descript;
+  def getCredit = course_Credit;
+  def getLevel = course_Level;
+  def getDuration = course_Duration;
 
   def show() = {
-    println("Course ID: " + courseID)
-    println("Department ID: " + deptID)
-    println("Course Title: " + courseTitle)
-    println("Course Descript: " + courseDescript)
-    println("Course Credit: " + courseCredit)
-    println("Course Level: " + courseLevel)
-    println("Course Duration: " + courseDuration)
+    println("Course ID: " + course_ID)
+    println("Department ID: " + dept_ID)
+    println("Course Title: " + course_Title)
+    println("Course Descript: " + course_Descript)
+    println("Course Credit: " + course_Credit)
+    println("Course Level: " + course_Level)
+    println("Course Duration: " + course_Duration)
 
 
   }
