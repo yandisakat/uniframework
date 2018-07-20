fbc01902e1525eb2944f6a1e9d5b9b0c2281ee5c
Dorcas Mbi <24913040+dorcasmbi@users.noreply.github.com>
2018-07-20 11:46:19 +0200

    Department Controller

---
+++ app/controllers/DepartmentController.scala
@@ -0,0 +1,13 @@
+package controllers.department
+
+import javax.inject.Inject
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+/**
+  * Created Dorcas on 2018/07/18.
+  */
+class DepartmentController @Inject()(cc: ControllerComponents) extends AbstractController(cc)  {
+
+
+}
+
