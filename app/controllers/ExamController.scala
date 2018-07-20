4f77210c744db81803ad2543befc057a9a1e3e8f
Dorcas Mbi <24913040+dorcasmbi@users.noreply.github.com>
2018-07-20 11:44:14 +0200

    Exam Controller class uploaded

---
+++ app/controllers/ExamController.scala
@@ -0,0 +1,13 @@
+package controllers.exam
+
+import javax.inject.Inject
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+/**
+  * Created Dorcas on 2018/07/18.
+  */
+class ExamController @Inject()(cc: ControllerComponents) extends AbstractController(cc)  {
+
+
+}
+
