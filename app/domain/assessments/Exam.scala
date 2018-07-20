b5ccceafa8a572a29e7db39630fe630b1b155ccf
Dorcas Mbi <24913040+dorcasmbi@users.noreply.github.com>
2018-07-20 12:10:54 +0200

    Add files via upload

---
+++ app/domain/assessments/Exam.scala
@@ -0,0 +1,19 @@
+import play.api.libs.json.Json
+/*
+ * @param subject name
+ * @param venue no the number of the venue
+ * @param exam time
+ * @param exam controller
+ * @param exam date
+ *
+ */
+case class Exam(subject_name:String, venue_no:String, exam_time:String, exam_controller:String, exam_date:String)
+
+object Exam extends App
+{
+
+  implicit val exam = Json.format[Exam]
+
+  def identity: Exam = Exam("", "","","","")
+
+}
