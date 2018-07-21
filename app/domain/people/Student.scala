b0f2b1475283789417df551b4d6bd75c3785cd78
Yandisa <38737545+yandisakat@users.noreply.github.com>
2018-07-21 22:13:50 +0200

    Update of Student.scala file

--- app/domain/people/Student.scala
+++ app/domain/people/Student.scala
@@ -1,5 +1,14 @@
 package domain.people
+import play.api.libs.json.Json
+/**
+  * By Yandisa Katiya - 2018/07/21
+  */
 
-class Student(id: String, firstname:String) {
 
+case class Student(val studentId: String, val deptId: String, val firstName:String, val lastName: String, val initial: String, val email: String) {
+
+  object Student
+  {
+    implicit val studentJson = Json.format[Lecturer]
+  }
 }
