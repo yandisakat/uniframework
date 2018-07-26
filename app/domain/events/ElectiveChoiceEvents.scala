f5a8fb2a199e92855370c0b95aaf51e1436e8723
Kessel Okinga Koumou <216012759@mycput.ac.za>
2018-07-26 07:46:41 +0200

    Elective choice Events

--- app/domain/events/ElectiveChoiceEvents.scala
+++ app/domain/events/ElectiveChoiceEvents.scala
@@ -1,9 +1,8 @@
+package domain.event
 
 import java.time.LocalDateTime
 
-abstract class ElectiveChoiceEvents(val studentID: Int, val subjectID: String, val eventData: LocalDateTime)
-
-{
+ class ElectiveChoiceEvents(val studentID: Int, val subjectID: String, val eventData: LocalDateTime) {
   var student_ID: Int = studentID
   var subject_ID: String = subjectID
   var event_Data: LocalDateTime
@@ -11,24 +10,31 @@ abstract class ElectiveChoiceEvents(val studentID: Int, val subjectID: String, v
   def getStudent_ID(): Int = {
     return this.student_ID
   }
+
   def getSubject_ID(): String = {
     return this.subjectID
   }
-  def getEvent_Data(): LocalDateTime ={
+
+  def getEvent_Data(): LocalDateTime = {
+
     return this.event_Data
   }
-  def choice()= {
 
-    println("Student ID: "+student_ID)
-    println("Subject ID: "+subject_ID)
-    println("Event Data and Time: "+event_Data)
+  def choice(): Unit = {
+
+    println(s"Student ID: ${student_ID}");
+    println(s"Subject ID: ${subject_ID}");
+    println(s"Event Data and Time: ${event_Data}");
+
   }
+
 
 }
 
-
 object ElectiveChoiceEvents extends App
 {
-  var elChoice = new ElectiveChoiceEvents (216012734, "ICE3213", "12/05/2018 09:00" );
-  elChoice.choice();
+  var elChoice = new ElectiveChoiceEvents (216012734, "ICE3213", LocalDateTime.now()) {
+    elChoice.choice()
+  }
+
 }
