885f6e63b4886792d39dcfb20374e783c530cab3
Kessel <33414897+Koumou@users.noreply.github.com>
2018-07-29 17:36:59 +0200

    Update Venue.scala

--- app/domain/places/Venue.scala
+++ app/domain/places/Venue.scala
@@ -1,5 +1,16 @@
 package domain.places
 
+
+
+/*
+ * @created by Katiya 
+ * @param Venue ID
+ * @param Venue name
+ 
+ --Complete the documentation
+ *
+ */
+
 import play.api.libs.json.Json
 
 case class Venue(venueId:String, venueName: String, entityId:String, capacity: Int)
