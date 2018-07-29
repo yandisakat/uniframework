0f7ec0af2642c76adc42f449db09336b7d712ea1
Kessel <33414897+Koumou@users.noreply.github.com>
2018-07-29 17:33:49 +0200

    Update and rename Lecturer to Lecturer.scala

---
+++ app/domain/people/Lecturer.scala
@@ -0,0 +1,22 @@
+package domain.people
+
+
+
+/*
+
+* @ Created by Reece
+
+--Ducumentation
+
+
+*/
+
+import play.api.libs.json.Json
+
+case class Lecturer(val firstname: String, val lastname: String, val telNo: String, val emailAddr: String, val classes: List[Subject_Class])
+
+object Lecturer{
+  implicit val lecturerJson = Json.format[Lecturer]
+  
+  
+}
