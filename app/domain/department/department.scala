a1e0f0fbac8bdf3de9275b573adbe4a496a89379
Kessel <33414897+Koumou@users.noreply.github.com>
2018-07-29 17:11:48 +0200

    Update department.scala

--- app/domain/department/department.scala
+++ app/domain/department/department.scala
@@ -5,3 +5,19 @@ Created by Dorcars on
 --Documentation
 
 */
+
+import play.api.libs.json.Json
+/*
+ * @param dpId department ID
+ * @param dpName department name
+ */
+
+case class Department(dpId: String, dpName: String)
+
+object Department extends  App
+{
+  implicit val department = Json.format[Department]
+
+  def identity: Department = Department("", "")
+
+}
