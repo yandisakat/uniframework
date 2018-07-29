155a492ab6d4c139f46c2251ec6e681b2850602e
Reece1994 <33478750+Reece1994@users.noreply.github.com>
2018-07-29 19:33:45 +0200

    Update Lecturer.scala

--- app/domain/people/Lecturer.scala
+++ app/domain/people/Lecturer.scala
@@ -1,20 +1,28 @@
-package domain.people
+package domain.people;
 
+import play.api.libs.json.Json;
 
-/**
- *
- * @created by Reece
-
---Ducumentation
-
+/** 
+ * @created by Reece Waldeck
+ * @param lect_id primary key to identify Lecturer
+ * @param nme
+ * @param srnme
+ * @param telNo
+ * @param emalAddr
+ * @param subjects the list of subjects the Lecturer lectures
  */
 
-import play.api.libs.json.Json
+case class Lecturer(
+    lect_id: Integer, 
+    nme: String, 
+    srnme: String, 
+    telNo: String, 
+    emailAddr: String,
+    subjects: List[Subject]
+)
 
-case class Lecturer(val firstname: String, val lastname: String, val telNo: String, val emailAddr: String, val classes: List[Subject_Class])
-
-object Lecturer{
-  implicit val lecturerJson = Json.format[Lecturer]
-  
-  
+object Lecturer
+{
+  implicit val lectFmt = Json.format[Lecturer];
+  def identity: Lecturer = Lecturer(0, "", "", "", "", null);
 }
