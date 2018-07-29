82fa3c496146ba8974a2c76a81efedff852b8c08
Kessel Okinga Koumou <216012759@mycput.ac.za>
2018-07-29 22:36:18 +0200

    Elective choice Events

--- app/domain/events/ElectiveChoiceEvents.scala
+++ app/domain/events/ElectiveChoiceEvents.scala
@@ -1,44 +1,23 @@
+package domain.events
 
 import java.time.LocalDateTime
 
- class ElectiveChoiceEvents(val student_ID: Int,
-                            val subject_ID: String,
-                            val event_Data: LocalDateTime)
- {
 
-   var studentID: Int = student_ID
-   var subjectID: String = subject_ID
-   var eventData: LocalDateTime = event_Data
-
-  def getStudent_ID(): Int = {
-    return this.studentID;
-  }
-
-  def getSubject_ID(): String = {
-    return this.subjectID;
-  }
-
-  def getEvent_Data(): LocalDateTime = {
-
-    return this.eventData;
-  }
-
-  def choice()={
-
-    println("Student ID: " + studentID)
-    println("Subject ID: " + subjectID)
-    println("Event Data and Time: " + eventData)
+/**
+  * created by Kessel
+  * @param student_ID student unique ID
+  * @param subject_ID subject unique ID
+  * @param event_Date
+  */
 
 
-  }
 
 
-}
+case class ElectiveChoiceEvents( student_ID:Int, subject_ID:String, event_Date:LocalDateTime)
 
-object ElectiveChoiceEvents extends App
-{
-  var elChoice = new ElectiveChoiceEvents (216012734, "ICE3213", LocalDateTime.now()) {
-    elChoice.choice()
-  }
+
+object ElectiveChoiceEvents {
+  implicit val ElectiveC = Json.format[ElectiveChoiceEvents]
+  def identify: ElectiveChoiceEvents = ElectiveChoiceEvents (0,"",LocalDateTime.now());
 
 }
