60b776eeb5d1944ba9e320ede6f1969e0ff36185
Kessel <33414897+Koumou@users.noreply.github.com>
2018-07-29 17:24:46 +0200

    Update and rename Faculty to Faculty.scala

---
+++ app/domain/courses/Faculty.scala
@@ -0,0 +1,22 @@
+package domain.courses
+
+
+/*
+ * @created by Reece 
+ * @param faculty ID
+ * @param faculty name
+ * @param faculty desscription
+ *
+ */
+
+
+import play.api.libs.json.Json
+
+case class Faculty(id:Int, name:String, room: String, description:String)
+
+object Faculty{
+
+  implicit val facultyJson = Json.format[Faculty]
+  def identity:Faculty = Faculty("","","","")
+  
+}
