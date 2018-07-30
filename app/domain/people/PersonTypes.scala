0f850aff22736ce17fda17fccff175e5de361f9b
Queengamer <33478834+Queengamer@users.noreply.github.com>
2018-07-30 18:44:42 +0200

    Update PersonTypes.scala

--- app/domain/people/PersonTypes.scala
+++ app/domain/people/PersonTypes.scala
@@ -1,5 +1,14 @@
+
 package domain.people
+import play.api.libs.json.Json
+/**
+  *@created Natasha
+  */
 
-case class PersonTypes() {
 
+case class PersonTypes(personId: String, deptId: String, firstName:String, lastName: String, initial: String, email: String) 
+{
+  object PersonTypes
+  {
+    implicit val PersonTypesJson = Json.format[PersonTypes]
 }
