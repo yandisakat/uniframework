03d5c5abef25dcef168c497a3bc0219a6eac6db8
MillerThomas <33414062+MillerThomas@users.noreply.github.com>
2018-07-30 22:13:50 +0200

    Update StudentCourse.scala

--- app/domain/courses/StudentCourse.scala
+++ app/domain/courses/StudentCourse.scala
@@ -1,5 +1,26 @@
 package domain.courses
+import java.time.LocalDateTime
+import play.api.libs.json.Json
 
-class StudentCourse {
+/**
+* Created by 216074630 on 7/30/2018.
+* Thomas Miller
+* @param subject id
+* @param subject name
+* @param description
+* @param course
+*/
+
+class SubjectCourse(
+    id:Int,
+	subjectName:String,
+	description:String,
+	course:String) 
+		
+object SubjectCourse{
+  
+  implicit val SubjectCourseFmt = Json.format[SubjectCourse]
+  def identity: SubjectCourse = SubjectCourse("",None,None,"")
+  
 
 }
