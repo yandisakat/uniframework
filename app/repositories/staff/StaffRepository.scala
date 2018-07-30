ca6d745f6d28182f302758e4abb5c14d761058b4
Queengamer <33478834+Queengamer@users.noreply.github.com>
2018-07-30 22:31:42 +0200

    Create StaffRepository.scala

---
+++ app/repositories/staff/StaffRepository.scala
@@ -0,0 +1,14 @@
+package repositories.staff
+
+import com.outworkers.phantom.dsl._
+import conf.connections.DataConnection
+import repositories.places.tables.StaffTableImpl
+
+class StaffDatabase(override val connector: KeySpaceDef) extends Database[StaffDatabase](connector)
+{
+  object StaffDatabase extends StaffDatabase(DataConnection.connector)
+  
+  trait StaffRepository
+  {
+    def database = StaffDatabase
+}
