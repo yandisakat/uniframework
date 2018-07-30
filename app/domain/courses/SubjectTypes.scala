6c42c98f43d5ef0312b0a4b96713c278454da5fc
Queengamer <33478834+Queengamer@users.noreply.github.com>
2018-07-30 18:41:16 +0200

    Update SubjectTypes.scala

--- app/domain/courses/SubjectTypes.scala
+++ app/domain/courses/SubjectTypes.scala
@@ -1,5 +1,14 @@
 package domain.courses
 
-class SubjectTypes {
+import play.api.libs.json.Json
 
+/**
+  *@created by Natasha
+  */
+
+case class SubjectTypes(id:String, name:String, description:String,subjects:List[String])
+
+object SubjectTypes{
+  implicit  val SubjectTypesForm = Json.format[SubjectTypes]
+  def identity:SubjectTypes = SubjectTypes("","","", Nil)
 }
