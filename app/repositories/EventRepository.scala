4a979089a0d188ba4c3fa22ba116eb61be119625
Queengamer <33478834+Queengamer@users.noreply.github.com>
2018-07-30 17:13:00 +0200

    Create EventRepository.scala

---
+++ app/repositories/EventRepository.scala
@@ -0,0 +1,14 @@
+package repositories.places
+
+import com.outworkers.phantom.dsl._
+import conf.connections.DataConnection
+import repositories.places.tables.EventTableImpl
+
+class EventDatabase(override val connector: KeySpaceDef) extends Database[EventDatabase](connector)
+{
+  object EventDatabase extends EventDatabase(DataConnection.connector)
+  
+  trait EventRepository
+  {
+    def database = EventDatabase
+}
