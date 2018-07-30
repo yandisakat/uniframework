fa5b8815a0af46a172ee08873ff9a844139c9e58
Queengamer <33478834+Queengamer@users.noreply.github.com>
2018-07-30 22:27:56 +0200

    Update Entity.scala

--- app/domain/places/Entity.scala
+++ app/domain/places/Entity.scala
@@ -1,8 +1,10 @@
 package domain.places
 
 //represents University, Faculty and Department
-case class Entity()
+import play.api.libs.json.Json
 
-{
+case class Entity(entityId:String, entityName: String, universiId:String, facultyId:String, departmentId:String)
 
+object Entity{
+  implicit val entityJson = Json.format[Entity]
 }
