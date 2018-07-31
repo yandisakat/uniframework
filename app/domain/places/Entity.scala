8b4b4e4de7d3be11b87646b344408c061c6f344a
Kessel Okinga Koumou <216012759@mycput.ac.za>
2018-07-31 21:41:44 +0200

    Documentation

--- app/domain/places/Entity.scala
+++ app/domain/places/Entity.scala
@@ -3,11 +3,9 @@ package domain.places
 
 /**
   * created by Natasha
-  * @param entityid
-  * @param entityName
-  * @param universiId
-  * @param facultyId
-  * @param departmentId
+  *
+  *
+  *
   *
   */
 
@@ -15,8 +13,9 @@ package domain.places
 //represents University, Faculty and Department
 import play.api.libs.json.Json
 
-case class Entity(entityId:String, entityName: String, universiId:String, facultyId:String, departmentId:String)
+case class Entity(entityId: String, entityName: String, universiId: String, facultyId: String, departmentId: String)
 
 object Entity{
   implicit val entityJson = Json.format[Entity]
+  def definity: Entity = Entity ("","","","","")
 }
