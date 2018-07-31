72e8894d4079eab06dab6897ff2b43c2dd6dd807
Walied12 <33478150+Walied12@users.noreply.github.com>
2018-07-31 21:29:27 +0200

    Update StudentSubjects.scala

--- app/domain/courses/StudentSubjects.scala
+++ app/domain/courses/StudentSubjects.scala
@@ -1,5 +1,33 @@
-package domain.courses
+package domain.users
 
-class StudentSubjects {
+import java.time.LocalDateTime
+import play.api.libs.json.Json
 
+/**
+ * 
+ * @param orgId
+ * @param email
+ * @param firstName
+ * @param lastName
+ * @param screenName
+ * @param password
+ * @param state
+ * @param date
+ */
+
+case class StudentSubject(orgId: String,
+                          email: String,
+                          firstName: Option[String],
+                          lastName: Option[String],
+                          screenName: String,
+                          password: String,
+                          state: String,
+                          date: LocalDateTime)
+
+
+object StudentSubject{
+  
+  implicit val studentSubjectFmt = Json.format[StudentSubject]
+  def identity: StudentSubject = StudentSubject("","",None,None,"","","",LocalDateTime.now())
+  
 }
