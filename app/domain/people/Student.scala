1a00030b580af8b2374ea7d422159a2930d80085
MillerThomas <33414062+MillerThomas@users.noreply.github.com>
2018-08-02 10:11:55 +0200

    Update Student.scala

--- app/domain/people/Student.scala
+++ app/domain/people/Student.scala
@@ -7,16 +7,17 @@ import play.api.libs.json.Json
  * @param Last Name
  * @param Initial
  * @param Email Address
+ * @param Phone 
  *
  *By Yandisa Katiya - 2018/07/21
  *
  */
 
-case class Student(studentId: String, deptId: String, firstName:String, lastName: String, initial: String, email: String) 
+case class Student(studentId: String, deptId: String, firstName:String, lastName: String, initial: String, email: String, phone: String) 
 {
   object Student
   {
     implicit val studentJson = Json.format[Lecturer]
-    def identity: Student = Student("","","","","","")
+    def identity: Student = Student("","","","","","","")
   }
 }
