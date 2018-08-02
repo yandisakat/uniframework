83d530a28fce1f6da7e76063e07c4c33bcd99cff
MillerThomas <33414062+MillerThomas@users.noreply.github.com>
2018-08-02 10:47:23 +0200

    Update Subject.scala

--- app/domain/courses/Subject.scala
+++ app/domain/courses/Subject.scala
@@ -5,13 +5,14 @@ package domain.courses
   * @param SubjectId
   * @param name
   * @param credits
+  * @param description
   */
 
-case class Subject(SubjectId: String, name:String, credits: Double)
+case class Subject(SubjectId: String,description: String, name:String, credits: Double)
 
 object Subject {
 
   implicit val s = Json.format[Subejct]
-  def identify:Subject = Subject ("","",0.0)
+  def identify:Subject = Subject ("","","",0.0)
 
 }
