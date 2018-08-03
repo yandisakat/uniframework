a7269a1b168e48ed35800872e638d3e12951fc18
Yandisa <38737545+yandisakat@users.noreply.github.com>
2018-08-03 23:20:41 +0200

    Review of Lecturer.scala

--- app/domain/people/Lecturer.scala
+++ app/domain/people/Lecturer.scala
@@ -4,11 +4,12 @@ import play.api.libs.json.Json;
 
 /** 
  * created by Reece Waldeck
+ * Part 1 - reviewed by Yandisa
  * @param lect_id primary key to identify Lecturer
  * @param nme
  * @param srnme
  * @param telNo
- * @param emalAddr
+ * @param emailAddr
  * @param Address 
  * @param subjects the list of subjects the Lecturer lectures
  */
@@ -27,6 +28,6 @@ object Lecturer
 {
   implicit val lectFmt = Json.format[Lecturer];
 
-  def identity: Lecturer = Lecturer("", "", "", "", "", null);
+  def identity: Lecturer = Lecturer("", "", "", "", "", "", null);
 
 }
