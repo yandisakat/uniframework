5618fe4aefb46290423df23c7f3f99318c1de4fa
Reece1994 <33478750+Reece1994@users.noreply.github.com>
2018-08-04 16:03:52 +0200

    Update Lecturer.scala

--- app/domain/people/Lecturer.scala
+++ app/domain/people/Lecturer.scala
@@ -1,26 +1,25 @@
 package domain.people;
-
 import play.api.libs.json.Json;
 
 /** 
  * created by Reece Waldeck
  * Part 1 - reviewed by Yandisa
- * @param lect_id primary key to identify Lecturer
+ * @param lectId primary key to identify Lecturer
  * @param name
- * @param surname
- * @param telephone number 
- * @param EMail Address 
- * @param Address 
+ * @param srname
+ * @param telNo
+ * @param emailAddr 
+ * @param addr 
  * @param subjects the list of subjects the Lecturer lectures
  */
 
 case class Lecturer(
     lectId: String, 
-    nme: String, 
-    srnme: String, 
+    name: String, 
+    srname: String, 
     telNo: String, 
     emailAddr: String,
-    Addr: String,
+    addr: String,
     subjects: List[Subject]
 )
 
