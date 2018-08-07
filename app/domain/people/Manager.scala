4e020d088dd124becba3b03d900ae8add9576460
Kessel <33414897+Koumou@users.noreply.github.com>
2018-08-07 17:11:58 +0200

    Update Manager.scala

--- app/domain/people/Manager.scala
+++ app/domain/people/Manager.scala
@@ -1,3 +1,25 @@
+package domain.people;
+import play.api.libs.json.Json;
+
+/** 
+ * created by Kessel
+ * Part 1 - reviewed by Yandisa
+ * @param lectId primary key to identify Lecturer
+ * @param name
+ * @param srname
+ * @param telNo
+ * @param emailAddr 
+ * @param addr 
+ * @param subjects the list of subjects the Lecturer lectures
+ */
 
 
-, managerStartDate: LocalDateTime, managerEndDate: LocalDateTime
+case class Manager(managerID: String, firstname: String, lastname: String, telNo: Int, emailAddr: String, addr: String, managerStartDate: LocalDateTime, managerEndDate: LocalDateTime)
+
+object Manager
+{
+  implicit val managerFormat = Json.format[Manager];
+
+  def identity: Manager = Manager("","","",0,"",);
+
+}
