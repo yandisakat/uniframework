63154311c55b34d779105f3f811e4e6998506076
Queengamer <33478834+Queengamer@users.noreply.github.com>
2018-08-07 22:29:41 +0200

    Update VenueRepository.scala

--- app/repositories/places/VenueRepository.scala
+++ app/repositories/places/VenueRepository.scala
@@ -1,1 +1,15 @@
+package repositories.places
 
+import com.outworkers.phantom.dsl._
+import conf.connections.DataConnection
+import repositories.users.tables.VenueTableImpl
+
+class VenueDatabase(override val connector: KeySpaceDef) extends Database[VenueDatabase](connector) {
+  object VenueTable extends VenueTableImpl with connector.Connector
+}
+
+object VenueDatabase extends VenueDatabase(DataConnection.connector)
+
+trait VenueRepository {
+  def database = VenueDatabase
+}
