6d7b1ab81afce7fe2ce4ddb0ebb69b348b493e52
Yandisa <38737545+yandisakat@users.noreply.github.com>
2018-08-07 23:36:58 +0200

    Update DepartmentRepository.scala

--- app/repositories/department/DepartmentRepository.scala
+++ app/repositories/department/DepartmentRepository.scala
@@ -1,1 +1,23 @@
-Yandisa
+package repositories.department
+
+import com.outworkers.phantom.dsl._
+import conf.connections.DataConnection
+import repositories.department.tables._
+/*
+ *  By Yandisa Katiya 
+ */
+
+
+class DepartmentDatabase(override val connector: KeySpaceDef) extends Database[DepartmentDatabase](connector)
+{
+  object DepartmentTable extends DepartmentTableImp with connector.Connector
+}
+
+object DepartmentDatabase extends DepartmentDatabase(DataConnection.connector)
+
+trait DepartmentRepository
+{
+  def database = DepartmentDatabase
+}
+
+
