4ec511e08cf00bec3a8c05dd035b4520133366b9
Walied12 <33478150+Walied12@users.noreply.github.com>
2018-08-07 17:47:20 +0200

    Update StudentRepository.scala

--- app/repositories/people/StudentRepository.scala
+++ app/repositories/people/StudentRepository.scala
@@ -1,1 +1,15 @@
-wailed
+package repositories.people
+
+import com.outworkers.phantom.dsl._
+import conf.connections.DataConnection
+import repositories.people.tables.StudentTableImpl
+
+class StudentDatabase(override val connector: KeySpaceDef) extends Database[StudentDatabase](connector) {
+  object studentTable extends StudentTableImpl with connector.Connector
+}
+
+object StudentDatabase extends StudentDatabase(DataConnection.connector)
+
+trait StudentRepository {
+  def database = StudentDatabase
+}
