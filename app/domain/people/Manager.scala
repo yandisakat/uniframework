11a363ba8c95c7d54a9e2321c63ef766ea1f62aa
Kessel Okinga Koumou <216012759@mycput.ac.za>
2018-08-07 23:06:19 +0200

    Review part1

--- app/domain/people/Manager.scala
+++ app/domain/people/Manager.scala
@@ -5,24 +5,22 @@ import play.api.libs.json.Jso
 
 /**
   *
-  * created by Kessel
   * @param managerID
   * @param firstname
   * @param lastname
+  * @param dateofbirth
   * @param telNo
   * @param emailAddr
   * @param addr
   * @param managerStartDate
   * @param managerEndDate
-  *
   */
 
-
-case class Manager(managerID: String, firstname: String, lastname: String, telNo: Int, emailAddr: String, addr: String, managerStartDate: LocalDateTime, managerEndDate: LocalDateTime)
+case class Manager(managerID: String, firstname: String, lastname: String,dateofbirth: LocalDate, telNo: Int, emailAddr: String, addr: String, managerStartDate: LocalDateTime, managerEndDate: LocalDateTime)
 
 object Manager {
 
   implicit val managerFormat = Json.format[Manager]
-  def identity: Manager = Manager("","","",0,"","",LocalDateTime.now(),LocalDateTime.now())
+  def identity: Manager = Manager("","","",LocalDateTime.now(),0,"","",LocalDateTime.now(),LocalDateTime.now())
 
 }
