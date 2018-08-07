4323d0d65a5ce572cabdfc235547ab29bc9f4c0d
Yandisa <38737545+yandisakat@users.noreply.github.com>
2018-08-07 23:52:13 +0200

    Update StaffRepository.scala

--- app/repositories/people/StaffRepository.scala
+++ app/repositories/people/StaffRepository.scala
@@ -1,1 +1,22 @@
-Yandisa
+package repositories.people
+
+import com.outworkers.phantom.dsl._
+import conf.connections.DataConnection
+import repositories.people.tables._
+
+/*
+ * By Yandisa Katiya
+ */
+
+class StaffDatabase(override val connector: KeySpaceDef) extends Database(StaffDatabase](connector)
+{
+  object StaffTable extends StaffTableImpl with connector.Connector
+}
+                                                                          
+object StaffDatabase extends StaffDatabase(DataConnection.connector)
+                                                                          
+trait StaffRepository
+{
+  def database = StaffDatabase
+}
+                                                                            
