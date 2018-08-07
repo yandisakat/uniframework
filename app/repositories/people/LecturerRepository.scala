132d3acda9cee601ae7755386532669e0a5e8f3f
Kessel Okinga Koumou <216012759@mycput.ac.za>
2018-08-07 17:46:49 +0200

    Review part1

--- app/repositories/people/LecturerRepository.scala
+++ app/repositories/people/LecturerRepository.scala
@@ -1,1 +1,20 @@
-Kessel
+package repositories.people
+
+import com.outworkers.phantom.dsl._
+import conf.connections.DataConnection
+import repositories.people.tables.PeopleTablelmpl
+
+
+class peopleDatabase(override val connector: KeySpaceDef)extends Database [peopleDatabase](connect) {
+
+
+  object peopleTable extends peopleTableLmpl with connector.Connector
+
+}
+
+
+object peopleDatabase extends PeopleDatabase(DataConnection.connector)
+
+trait PeopleRepository {
+  def database = PeopleDatabase
+}
