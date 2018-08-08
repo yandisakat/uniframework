f79fe27c4fd227f84e1309e5bc233f2e4504ae3f
Yandisa <38737545+yandisakat@users.noreply.github.com>
2018-08-08 21:59:58 +0200

    Update MarkRepository.scala

--- app/repositories/assessment/MarkRepository.scala
+++ app/repositories/assessment/MarkRepository.scala
@@ -1,1 +1,22 @@
-Yandisa
+package repositories.assessment
+
+import com.outworkers.phantom.dsl._
+import conf.connections.DataConnection
+import repositories.assessment.tables._
+
+/*
+ * By Yandisa Katiya
+ */
+
+class MarkDatabase(override val connector: KeySpaceDef) extends Database[MarkDatabase](connector)
+{
+  object MarkTable extends MarkTableImpl with connector.Connector
+}
+
+object MarkDatabase extends MarkDatabase(DataConnection.connector)
+
+trait MarkRepository
+{
+   def database = MarkDatabase
+}
+
