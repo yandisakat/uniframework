d7a30321f55028e7f0c11895090d6ca4e22cb549
Yandisa <38737545+yandisakat@users.noreply.github.com>
2018-08-08 23:19:06 +0200

    Update SubjectTable.scala

--- app/repositories/courses/tables/SubjectTable.scala
+++ app/repositories/courses/tables/SubjectTable.scala
@@ -1,1 +1,55 @@
+package repositories.courses.tables
 
+/*
+ * By Yandisa Katiya
+ */
+
+import com.outworkers.phantom.dsl._
+import com.outworkers.phantom.jdk8._
+import com.outworkers.phantom.streams._
+import domain.courses.Subject
+import scala.concurrent.Future
+
+abstract class SubjectTable extends Table[SubjectTable, Subject]
+{
+  object subjectId extends StringColumn with PartitionKey
+ 
+  object subjectName extends StringColumn with PrimaryKey with ClusteringOrder with Ascending
+  
+  object subjectCredits extends StringColumn
+  
+  object subjectDuration extends StringColumn
+  
+  object minPassPercent extends DoubleColumn
+  
+}
+
+abstract class SubjectTableImpl extends SubjectTable with RootConnector
+{
+    override lazy val tableName = "subject"
+    
+    def save(role: subject): Future[ResultSet] =
+    {
+      insert
+        .value(_.subjectId, role.subjectId)
+        .value(_.subjectName, role.subjectName) 
+        .future()
+    }
+  
+    def getSubject(subjectId:String, subjectName:String): Future[Seq[Subject]] =
+    {
+       select 
+          .where(_.subjectId eqs subjectId)]
+          .and(subjectName eqs subjectName)
+          .fetchEnumerator() run Iteratee.collect()
+    }
+  
+    def deleteSubject(subjectId:String, subjectName:String):Future[ResultSet] =
+    {
+      delete
+        .where(_.subjectId eqs subjectId)
+        .and(_.subjectName eqs subjectName)
+        .future()
+    }
+  
+}
