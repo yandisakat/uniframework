fa2bd61818820d232325c04260c0f0e7f40dbc15
Reece1994 <33478750+Reece1994@users.noreply.github.com>
2018-08-08 20:19:26 +0200

    Update LecturerRepository.scala

--- app/repositories/people/LecturerRepository.scala
+++ app/repositories/people/LecturerRepository.scala
@@ -1,1 +1,15 @@
-Kessel
+package repositories.people
+
+import com.outworkers.phantom.dsl._
+import conf.connections.DataConnection
+import repositories.people.tables.LecturerTableImpl
+
+class LecturerDatabase(override val connector: KeySpaceDef) extends Database[LecturerDatabase](connector) {
+  object lecturerTable extends LecturerTableImpl with connector.Connector
+}
+
+object LecturerDatabase extends LecturerDatabase(DataConnection.connector)
+
+trait LecturerRepository {
+  def database = LecturerDatabase
+}
