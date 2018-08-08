48f4d014d4b3495c35d8ce08e877e4f08154eee4
Reece1994 <33478750+Reece1994@users.noreply.github.com>
2018-08-08 20:06:39 +0200

    Update FacultyRepository.scala

--- app/repositories/courses/FacultyRepository.scala
+++ app/repositories/courses/FacultyRepository.scala
@@ -1,1 +1,15 @@
-Dorcas
+package repositories.courses
+
+import com.outworkers.phantom.dsl._
+import conf.connections.DataConnection
+import repositories.courses.tables.FacultyTableImpl
+
+class FacultyDatabase(override val connector: KeySpaceDef) extends Database[FacultyDatabase](connector) {
+  object facultyTable extends FacultyTableImpl with connector.Connector
+}
+
+object FacultyDatabase extends FacultyDatabase(DataConnection.connector)
+
+trait FacultyRepository {
+  def database = FacultyDatabase
+}
