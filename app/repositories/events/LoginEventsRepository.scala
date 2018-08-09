67b5ca888d1257b0eb16fabe4172187003bf25aa
Dorcas Mbi <24913040+dorcasmbi@users.noreply.github.com>
2018-08-09 22:35:05 +0200

    Update LoginEventsRepository.scala

--- app/repositories/events/LoginEventsRepository.scala
+++ app/repositories/events/LoginEventsRepository.scala
@@ -1,1 +1,20 @@
-Dorcas
+package repositories.events
+
+import com.outworkers.phantom.dsl._
+import conf.connections.DataConnection
+import repositories.events.tables._
+
+class PersonDatabase(override val connector: KeySpaceDef) extends Database[PersonDatabase](connector) {
+
+  object LoginEventTable extends LoginEventTableImpl with connector.Connector
+
+  object LoginTable extends LoginEventOrgTableImpl with connector.Connector
+
+
+}
+
+object LoginEventDatabase extends LoginEventDatabase(DataConnection.connector)
+
+trait LoginEventsRepository {
+  def database = LoginEventDatabase
+}
