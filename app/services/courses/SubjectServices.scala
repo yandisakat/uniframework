c0671a3bbec04cf581284d14cb15d9a4c679cb73
Reece1994 <33478750+Reece1994@users.noreply.github.com>
2018-08-10 02:23:28 +0200

    Create SubjectServices.scala

---
+++ app/services/courses/SubjectServices.scala
@@ -0,0 +1,22 @@
+package services.courses
+
+import com.datastax.driver.core.ResultSet
+import domain.courses.Subject
+import services.courses.Impl.SubjectServicesImpl
+
+import scala.concurrent.Future
+
+trait SubjectServices {
+
+  def save(subject: Subject): Future[ResultSet]
+
+  def getSubjects(subjId: String): Future[Seq[Subject]]
+
+  def getSubject(subjId: String): Future[Subject]
+
+  def deleteSubjects(subjId: String): Future[ResultSet]
+}
+
+object SubjectServices{
+  def apply: SubjectServices = new SubjectServicesImpl
+}
