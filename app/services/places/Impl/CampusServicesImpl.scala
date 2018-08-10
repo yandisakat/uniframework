c5f9d8f531704972fa4f60365d54988c9b322508
Reece1994 <33478750+Reece1994@users.noreply.github.com>
2018-08-10 02:52:51 +0200

    Create CampusServicesImpl.scala

---
+++ app/services/places/Impl/CampusServicesImpl.scala
@@ -0,0 +1,34 @@
+package services.places.Impl
+
+import com.datastax.driver.core.ResultSet
+import domain.places.Campus
+import repositories.places.CampusRepository
+import services.places.CampusServices
+
+import scala.concurrent.Future
+
+class CampusServicesImpl extends CampusRoleRepository with CampusServices{
+  def save(campus: Campus): Future[ResultSet] = {
+    database.campusRoleTable.save(campus)
+
+  }
+
+  def getCampuses(campusId: String): Future[Seq[Campus]] = {
+    database.campusTable.getCampuses(campusId)
+
+  }
+
+  def getCampus(campusId: String): Future[Campus] = {
+    database.campusTable.getCampuses(campusId).map(campus => campus.head)
+  }
+
+  def deleteCampuses(campusId:String):Future[ResultSet] ={
+    database.campusTable.deleteCampuses(campusId)
+  }
+
+}
+
+object CampusServicesImpl extends CampusServicesImpl with CampusRepository
+
+
+}
