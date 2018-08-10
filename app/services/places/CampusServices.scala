7d79fa42e0007aab2badb04c385cd60f19a711ec
Reece1994 <33478750+Reece1994@users.noreply.github.com>
2018-08-10 02:42:17 +0200

    Create CampusServices.scala

---
+++ app/services/places/CampusServices.scala
@@ -0,0 +1,22 @@
+package services.places
+
+import com.datastax.driver.core.ResultSet
+import domain.places.Campus
+import services.places.Impl.CampusServicesImpl
+
+import scala.concurrent.Future
+
+trait CampusServices {
+
+  def save(campus: Campus): Future[ResultSet]
+
+  def getCampus(campusId: String): Future[Seq[Campus]]
+
+  def getCampus(campusId: String): Future[Campus]
+
+  def deleteCampuses(campusId: String): Future[ResultSet]
+}
+
+object CampusServices{
+  def apply: CampusServices = new CampusServicesImpl
+}
