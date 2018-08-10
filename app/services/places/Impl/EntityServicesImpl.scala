edaac857d713687e8766bfff51755be94ffd8b03
Reece1994 <33478750+Reece1994@users.noreply.github.com>
2018-08-10 03:11:28 +0200

    Create EntityServicesImpl.scala

---
+++ app/services/places/Impl/EntityServicesImpl.scala
@@ -0,0 +1,34 @@
+package services.places.Impl
+
+import com.datastax.driver.core.ResultSet
+import domain.places.Entity
+import repositories.places.EntityRepository
+import services.places.EntityServices
+
+import scala.concurrent.Future
+
+class EntityServicesImpl extends EntityRepository with EntityServices{
+  def save(entity: Entity): Future[ResultSet] = {
+    database.entityTable.save(entity)
+
+  }
+
+  def getEntities(entityId: String): Future[Seq[Entity]] = {
+    database.entityTable.getEntities(entityId)
+
+  }
+
+  def getEntity(entityId: String): Future[Entity] = {
+    database.entityTable.getEntities(entityId).map(entity => entity.head)
+  }
+
+  def deleteEntities(entityId:String):Future[ResultSet] ={
+    database.entityTable.deleteEntities(entityId)
+  }
+
+}
+
+object EntityServicesImpl extends EntityServicesImpl with EntityRepository
+
+
+}
