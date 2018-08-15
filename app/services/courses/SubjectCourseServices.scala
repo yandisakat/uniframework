d813c33d76483fc645d4658ad757b969e3340985
Walied12 <33478150+Walied12@users.noreply.github.com>
2018-08-15 19:54:16 +0200

    Update SubjectCourseServices.scala

--- app/services/courses/SubjectCourseServices.scala
+++ app/services/courses/SubjectCourseServices.scala
@@ -1,6 +1,30 @@
+package services.courses
+
+import com.datastax.driver.core.ResultSet
+import domain.courses.SubjectCourseServices
+import services.courses.Impl.SubjectCourseServicesImpl
+
+import scala.concurrent.Future
+
 /** 
  * creater Wailed
  * reviewer Omega
  *
  *
 */
+
+
+trait SubjectCourseServices {
+
+  def save(subjectCourse: SubjectCourse): Future[ResultSet]
+
+  def getSubjectCourse(subjectId: String): Future[Seq[SubjectCourse]]
+
+  def getSubjectCourse(subjectId: String): Future[SubjectCourse]
+
+  def deleteSubjectCourses(subjectId: String): Future[ResultSet]
+}
+
+object SubjectCourseServices{
+  def apply: SubjectCourseServices = new SubjectCourseServicesImpl
+}
