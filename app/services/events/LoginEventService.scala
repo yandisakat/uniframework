d4155e2676965652df8cac24cb844b57b3dbb200
Walied12 <33478150+Walied12@users.noreply.github.com>
2018-08-15 22:53:50 +0200

    Update LoginEventService.scala

--- app/services/events/LoginEventService.scala
+++ app/services/events/LoginEventService.scala
@@ -1,5 +1,29 @@
+package services.events
+
+import com.datastax.driver.core.ResultSet
+import domain.events.LoginEvent
+import services.events.Impl.LoginEventServicesImpl
+
+import scala.concurrent.Future
+
 /**
   *
   * creater Wailed
   * reviewer Dorcas
   */
+
+
+trait LoginEventServices {
+
+  def save(loginEvent: LoginEvent): Future[ResultSet]
+
+  def getLoginEvent(username: String): Future[Seq[LoginEvent]]
+
+  def getLoginEvent(username: String): Future[LoginEvent]
+
+  def deleteLoginEvents(username: String): Future[ResultSet]
+}
+
+object LoginEventServices{
+  def apply: LoginEventServices = new LoginEventServicesImpl
+}
