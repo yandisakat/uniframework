1e44401ee29d5c91514ab5090883a413305c8762
Dorcas Mbi <24913040+dorcasmbi@users.noreply.github.com>
2018-08-16 11:43:13 +0200

    Update ExamService.scala

--- app/services/assessments/ExamService.scala
+++ app/services/assessments/ExamService.scala
@@ -3,3 +3,26 @@
  * reviewer Reece
  *
  */
+package services.assessments
+
+import com.datastax.driver.core.ResultSet
+import domain.assessments.Exam
+import services.assessments.Impl.ExamServicesImpl
+
+import scala.concurrent.Future
+
+trait ExamService {
+
+  def save(exam: Exam): Future[ResultSet]
+
+  def getExamDetails(examId: String): Future[Seq[Exam]]
+
+  def getExamDetails(examId: String): Future[Exam]
+
+  def deleteExam(examId: String): Future[ResultSet]
+}
+
+object ExamService{
+  def apply: ExamService = new ExamServiceImpl
+}
+
