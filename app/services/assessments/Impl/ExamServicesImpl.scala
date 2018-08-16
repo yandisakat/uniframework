d8f0e758118bc6d74cbaa217b0577d3652573e62
Dorcas Mbi <24913040+dorcasmbi@users.noreply.github.com>
2018-08-16 11:48:16 +0200

    Update and rename ExamImpl.scala to ExamServicesImpl.scala

---
+++ app/services/assessments/Impl/ExamServicesImpl.scala
@@ -0,0 +1,19 @@
+/**
+ * creater Dorcas
+ * reviewer Reece
+ *
+ */
+package services.assessments.Impl
+
+import com.datastax.driver.core.ResultSet
+import domain.assessments.Exam
+import repositories.assessment.ExamRepository
+import services.assessments.ExamService
+
+import scala.concurrent.Future
+
+class ExamServicesImpl extends ExamRepository with ExamService{
+  def save(exam: Exam: Future[ResultSet] = {
+    database.ExamTable.save(exam)
+
+  }
