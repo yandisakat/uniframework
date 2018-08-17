ddaa581354a37cbacf69feb20caa96377adadf48
Reece1994 <33478750+Reece1994@users.noreply.github.com>
2018-08-18 00:11:25 +0200

    Update StudentServices.scala

--- app/services/people/StudentServices.scala
+++ app/services/people/StudentServices.scala
@@ -1,5 +1,22 @@
-/**
- * creater Reece
- * reviewer Kessel
- *
- */
+package services.people
+
+import com.datastax.driver.core.ResultSet
+import domain.people.Student
+import services.people.Impl.StudentServicesImpl
+
+import scala.concurrent.Future
+
+trait StudentServices {
+
+  def save(student: Student): Future[ResultSet]
+
+  def getStudents(studentId: String): Future[Seq[Student]]
+
+  def getStudent(studentId: String): Future[Student]
+
+  def deleteStudents(studentd: String): Future[ResultSet]
+}
+
+object StudentServices{
+  def apply: StudentServices = new StudentServicesImpl
+}
