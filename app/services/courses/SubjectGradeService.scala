15e7503cf984c802f3a7d83ffb89d8abe8554c65
Queengamer <33478834+Queengamer@users.noreply.github.com>
2018-08-18 12:53:47 +0200

    Update SubjectGradeService.scala

--- app/services/courses/SubjectGradeService.scala
+++ app/services/courses/SubjectGradeService.scala
@@ -1,11 +1,27 @@
+package services.courses
+
 /** 
  * created by Natasha
  * reviewer Omega
- *
- *
 */
-package services.courses
+
+import com.datastax.driver.core.ResultSet
+import domain.courses.SubjectGrade
+import services.courses.Impl.SubjectGradeServicesImpl
+
+import scala.concurrent.Future
 
 trait SubjectGradeServices {
 
+  def save(role: SubjectGrade): Future[ResultSet]
+
+  def getUserRoles(subjectId: String): Future[Seq[SubjectGrade]]
+
+  def getUserRole(subjectId: String): Future[SubjectGrade]
+
+  def deleteUserRoles(subjectId: String): Future[ResultSet]
+}
+
+object SubjectGradeServices{
+  def apply: SubjectGradeServices = new SubjectGradeServicesImpl
 }
