0705a83f4a17634167b3e958b61b1623a8d2b732
omerio9 <33478787+omerio9@users.noreply.github.com>
2018-08-18 12:25:31 +0100

    Update CourseRepository.scala

--- app/repositories/courses/CourseRepository.scala
+++ app/repositories/courses/CourseRepository.scala
@@ -1,1 +1,22 @@
-wailed
+package repositories.courses
+
+import com.outworkers.phantom.dsl._
+import conf.connections.DataConnection
+import repositories.courses.tables._
+
+/*
+*By Omega Mbadu
+*/
+
+class CourseDatabase(overide val connector: KeySpaceDef) extends Database [CourseDatabase](connector)
+{
+    object CourseTable extends CourseTalbleImp with connector.Connector
+}
+
+object CourseDatabase extends CourseDatabase(DataConnection.connector)
+
+trait CourseRepository
+{
+def database=CourseDatabase
+}
+
