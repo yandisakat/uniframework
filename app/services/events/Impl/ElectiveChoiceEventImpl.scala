57cf010120ec4c4b165de9e6e5ba5ce34bcf65cd
Queengamer <33478834+Queengamer@users.noreply.github.com>
2018-08-18 13:09:01 +0200

    Update ElectiveChoiceEventImpl.scala

--- app/services/events/Impl/ElectiveChoiceEventImpl.scala
+++ app/services/events/Impl/ElectiveChoiceEventImpl.scala
@@ -1,5 +1,39 @@
+package services.events.Impl
+
 /**
-  *
-  * creater Natasha
+  * created Natasha
   * reviewer Reece
   */
+
+import com.datastax.driver.core.ResultSet
+import domain.events.ElectiveChoiceEvent
+import repositories.events.ElectiveChoiceEventRepository
+import services.events.ElectiveChoiceEventServices
+
+import scala.concurrent.Future
+
+class ElectiveChoiceEventServicesImpl extends ElectiveChoiceEventRepository with ElectiveChoiceEventServices{
+  def save(role: ElectiveChoiceEvent): Future[ResultSet] = {
+    database.ElectiveChoiceEventTable.save(role)
+
+  }
+
+  def getElectiveChoiceEvent(electiveId: String): Future[Seq[ElectiveChoiceEvent]] = {
+    database.ElectiveChoiceEventTable.getElectiveChoiceEvent(electiveId)
+
+  }
+
+  def getElectiveChoiceEvent(electiveId: String): Future[ElectiveChoiceEvent] = {
+    database.ElectiveChoiceEventTable.getElectiveChoiceEvent(electiveId).map(role => role.head)
+  }
+
+  def deleteElectiveChoiceEvent(electiveId:String):Future[ResultSet] ={
+    database.ElectiveChoiceEventTable.deleteElectiveChoiceEvent(electiveId)
+  }
+
+}
+
+object ElectiveChoiceEventServicesImpl extends ElectiveChoiceEventServicesImpl with ElectiveChoiceEventRepository
+
+
+}
