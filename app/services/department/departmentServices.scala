77cd58ff910257cbc1a8c2baeab13e74c5f1852a
omerio9 <33478787+omerio9@users.noreply.github.com>
2018-08-18 14:13:25 +0100

    Update departmentServices.scala

--- app/services/department/departmentServices.scala
+++ app/services/department/departmentServices.scala
@@ -1,6 +1,29 @@
+package services.courses
 
 /**
-  *
-  * created by Omega
-  * reviewer Yandisa
-  */
+ * created by Omega Mbadu
+ * reviewer Yandisa
+ *
+ */
+
+import com.datastax.driver.core.ResultSet
+import domain.courses.Department
+import services.courses.Impl.DepartmentImpl
+
+import scala.concurrent.Future
+
+trait DepartmentServices 
+{
+  def save(dep: Department): Future[ResultSet]
+
+  def getDepartment(depId: String): Future[Seq[Department]]
+
+  def editDepartment(depId: String): Future[Department]
+
+  def deleteDepartment(depId: String): Future[ResultSet]
+}
+
+object DepartmentServices
+{
+  def apply: DepartmentServices = new DepartmentServicesImpl
+}
