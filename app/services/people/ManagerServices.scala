f3e41f00c90c9f7b4aaf6591ed0550fcc58855b6
omerio9 <33478787+omerio9@users.noreply.github.com>
2018-08-18 14:56:11 +0100

    Update ManagerServices.scala

--- app/services/people/ManagerServices.scala
+++ app/services/people/ManagerServices.scala
@@ -1,5 +1,31 @@
+package services.people
+
 /**
- * creater Omega
- * reviewer Thomas
+ * creator Omega 
+ * reviewer  Thomas
  *
  */
+
+import com.datastax.driver.core.ResultSet
+import domain.people.Manager
+import services.people.Impl.ManagerImpl
+
+import scala.concurrent.Future
+
+trait ManagerServices
+{
+  def save(manager: Manager): Future[ResultSet]
+  
+  def getManager(manId: String): Future[Seq[Manager]]
+  
+  def getManager(manId: String): Future[Manager]
+  
+  def deleteManager(manId: String): Future[ResultSet]
+  
+ }
+ 
+ object ManagerServices
+ {
+    def apply: ManagerServices = new ManagerServicesImpl
+    
+  }
