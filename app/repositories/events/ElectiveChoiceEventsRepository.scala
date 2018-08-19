5212dfd9d4ff51a0eb110a6f8b5107ed137d0017
Queengamer <33478834+Queengamer@users.noreply.github.com>
2018-08-19 12:45:29 +0200

    Update ElectiveChoiceEventsRepository.scala

--- app/repositories/events/ElectiveChoiceEventsRepository.scala
+++ app/repositories/events/ElectiveChoiceEventsRepository.scala
@@ -1,1 +1,22 @@
-wailed
+package repositories.events
+
+import com.outworkers.phantom.dsl._
+import conf.connections.DataConnection
+import repositories.events.tables._
+
+/*
+ * By Omega Mbadu
+ * reviewed by Natasha
+ */
+
+class ElectiveChoiceEventsDatabase(override val connector: KeySpaceDef) extends Database[ElectiveChoiceEventsDatabase](connector)
+{
+   object electiveChoiceEventsTable extends ElectiveChoiceEventsTableImp with connector.Connector
+}
+
+object ElectiveChoiceEventsDatabase extends ElectiveChoiceEventsDatabase(DataConnection.connector)
+
+trait ElectiveChoiceEventsRepository
+{
+  def database = ElectiveChoiceEventsDatabase
+}
