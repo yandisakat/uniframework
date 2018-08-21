74e4a76484242b665bf993c37076a5cadfebef29
Kessel <33414897+Koumou@users.noreply.github.com>
2018-08-21 12:19:27 +0200

    Update LecturerService.scala

--- app/services/people/LecturerService.scala
+++ app/services/people/LecturerService.scala
@@ -1,5 +1,28 @@
+package services.people
+
 /**
  * creater Kessel
  * reviewer Omega
  *
  */
+
+import com.datastax.driver.core.ResultSet
+import domain.people.Lecturer
+import services.people.Impl.RoleServicesImpl
+
+import scala.concurrent.Future
+
+trait LecturerServices {
+
+  def save(lecturer: Lecturer): Future[ResultSet]
+
+  def getLecture(lectId: String): Future[Seq[Lecturer]
+
+  def getLecture(lectId: String): Future[Lecturer]
+
+  def deleteLecturer(lectId: String): Future[ResultSet]
+}
+
+object LecturerServices{
+  def apply: LecturerServices = new LecturerServicesImpl
+}
