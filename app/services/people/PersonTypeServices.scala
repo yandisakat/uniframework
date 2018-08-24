db1822f30173fcaf3e94d10586a9acbde0fde4cd
Reece1994 <33478750+Reece1994@users.noreply.github.com>
2018-08-25 01:09:12 +0200

    Update PersonTypeServices.scala

--- app/services/people/PersonTypeServices.scala
+++ app/services/people/PersonTypeServices.scala
@@ -1,5 +1,28 @@
+package services.people
+
 /**
- * creater Omega
- * reviewer Natasha
+ * creater Kessel
+ * reviewer Omega
  *
  */
+
+import com.datastax.driver.core.ResultSet
+import domain.people.PersonType
+import services.people.Impl.PersonTypeServicesImpl
+
+import scala.concurrent.Future
+
+trait PersonTypesServices {
+
+  def save(personType: PersonType): Future[ResultSet]
+
+  def getPersonTypes(personTypeId: String): Future[Seq[PersonType]
+
+  def getPersonType(personTypeId: String): Future[PersonType]
+
+  def deletePersonType(personTypeId: String): Future[ResultSet]
+}
+
+object PersonTypeServices{
+  def apply: PersonTypeServices = new PersonTypeServicesImpl
+}
