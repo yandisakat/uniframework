c377aa1590166579a7a354343cfae6dd6ecf7397
Reece1994 <33478750+Reece1994@users.noreply.github.com>
2018-08-25 00:21:50 +0200

    Update SubjectTypesRepository.scala

--- app/repositories/courses/SubjectTypesRepository.scala
+++ app/repositories/courses/SubjectTypesRepository.scala
@@ -1,1 +1,17 @@
-Wailed
+package repositories.courses
+
+import com.outworkers.phantom.dsl._
+import conf.connections.DataConnection
+import repositories.courses.tables._
+
+class SubjectTypesDatabase(override val connector: KeySpaceDef) extends Database[SubjectTypesDatabase](connector)
+{
+   object SubjectTypesTable extends SubjectTypesTableImp with connector.Connector
+}
+
+object SubjectTypesDatabase extends SubjectTypesDatabase(DataConnection.connector)
+
+trait SubectTypesRepository
+{
+  def database = SubjectTypesDatabase
+}
